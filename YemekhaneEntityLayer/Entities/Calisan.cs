fc8841abbb8bbe36a553d71f03556711d9295921
Yamokay <yagmurkaya1212@gmail.com>
2025-07-17 13:19:32 +0300

    a

---
+++ YemekhaneEntityLayer/Entities/Calisan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YemekhaneEntityLayer.Entities
+{
+    public class Calisan
+    {
+        public  int calisanID { get; set; }
+        public bool aktiflik { get; set; }
+        public string calisanIsmi { get; set; }
+        public string calisanSoyad { get; set; }
+
+        public string calisanGorevi { get; set; }
+
+        public int calisanKartNo { get; set; }
+
+        //TODO 
+
+        public List<Okutma> Okutmalar { get; set; }
+
+
+
+
+    }
+}
---
+++ YemekhaneEntityLayer/Entities/Okutmalar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YemekhaneEntityLayer.Entities
+{
+    public class Okutmalar
+    {
+        public int OkutmaID { get; set; }
+        public Date OkutmaTarihi { get; set; }
+
+        public bool jokerGeçiş { get; set; }
+        public int gecisCount { get; set; }
+        public int jokerGecisCount { get; set; }
+    }
+}
