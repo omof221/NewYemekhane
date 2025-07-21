322bd05fcff4cae2a74648d3bc6a9dbbe5d77259
ömer yaşar <omeryasar745@gmail.com>
2025-07-21 10:06:59 +0300

    dfbhtf

--- PresentationLayer/Program.cs
+++ PresentationLayer/Program.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Microsoft.Extensions.Hosting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Extensions.DependencyInjection;
+
+
 
 namespace PresentationLayer
 {
@@ -14,9 +18,24 @@ namespace PresentationLayer
         [STAThread]
         static void Main()
         {
+            var host = CreateHostBuilder().Build();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
+            var form = host.Services.GetRequiredService<Form1>();
             Application.Run(new Form1());
+        }
+        static IHostBuilder CreateHostBuilder()
+        {
+            return Host.CreateDefaultBuilder()  
+                .ConfigureServices((context, services) =>
+                {
+                    // BURAYA ekleyeceksin
+                    //services.AddScoped<IAdminDal, EFAdminDal>();
+                    //services.AddScoped<>();
+                    services.AddScoped<Form1>(); // Form'u da ekle
+
+                    // Gerekirse diğer servis kayıtları
+                });
         }
     }
 }
