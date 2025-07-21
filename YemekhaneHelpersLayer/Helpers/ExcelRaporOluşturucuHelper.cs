6a2eea40cd92021a19048163dbb3c7998ff58e79
Yamokay <yagmurkaya1212@gmail.com>
2025-07-21 11:07:38 +0300

    helper model dolduruldu

--- YemekhaneHelpersLayer/Helpers/ExcelRaporOluşturucuHelper.cs
+++ YemekhaneHelpersLayer/Helpers/ExcelRaporOluşturucuHelper.cs
@@ -10,6 +10,27 @@ namespace YemekhaneHelpersLayer.Helpers
 {
     public class ExcelRaporOluşturucuHelper
     {
+        public static byte[] RaporuOlustur(List<Okutmalar> okutmalarListesi)
+        {
+            using var workbook = new XLWorkbook();
+            var ws = workbook.Worksheets.Add("Rapor");
+
+            ws.Cell(1, 1).Value = "Tarih";
+            ws.Cell(1, 2).Value = "Ad ";
+            ws.Cell(1, 3).Value = "Soyad";
+
+            for (int i = 0; i < okutmalarListesi.Count; i++)
+            {
+                var o = okutmalarListesi[i];
+                ws.Cell(i + 2, 1).Value = o.OkutmaTarihi.ToShortDateString();
+                ws.Cell(i + 2, 2).Value = o.calisan?.calisanIsmi ?? "";
+                ws.Cell(i + 2, 3).Value = o.calisan?.calisanSoyad ?? "";
+            }
+
+            using var stream = new MemoryStream();
+            workbook.SaveAs(stream);
+            return stream.ToArray();
+        }
 
     }
 }
