a2e02ecf2082f78722c27a3bcd104ed09265a293
Yamokay <yagmurkaya1212@gmail.com>
2025-07-21 10:07:18 +0300

    helpers and models folders added

--- YemekhaneHelpersLayer/Helpers/ExcelRaporOluşturucuHelper.cs
+++ YemekhaneHelpersLayer/Helpers/ExcelRaporOluşturucuHelper.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClosedXML.Excel;
+using YemekhaneEntityLayer.Entities;
 
 namespace YemekhaneHelpersLayer.Helpers
 {
-    public class ReportHelper
+    public class ExcelRaporOluşturucuHelper
     {
 
     }
