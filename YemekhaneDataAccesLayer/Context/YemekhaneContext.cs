df79315e65bb0d7898e26b61c2ac96ff4493703c
Yamokay <yagmurkaya1212@gmail.com>
2025-07-22 11:08:22 +0300

    ömere push

--- YemekhaneDataAccesLayer/Context/YemekhaneContext.cs
+++ YemekhaneDataAccesLayer/Context/YemekhaneContext.cs
@@ -16,6 +16,6 @@ namespace YemekhaneDataAccesLayer.Context
         public DbSet<Calisan> Calisanlar { get; set; }
         public DbSet<Okutmalar> Okutmalar { get; set; }
         public DbSet<Admin> Adminler { get; set; }
-    
+        public DbSet<YemekhaneCalisan>  yemekhaneCalisanlar{ get; set; }
     }
 }
