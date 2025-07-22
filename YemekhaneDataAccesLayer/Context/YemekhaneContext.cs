0052a492da56cf859bccdf536aa7eefe11d2366e
Yamokay <yagmurkaya1212@gmail.com>
2025-07-22 08:58:38 +0300

    pushhh

--- YemekhaneDataAccesLayer/Context/YemekhaneContext.cs
+++ YemekhaneDataAccesLayer/Context/YemekhaneContext.cs
@@ -9,18 +9,13 @@ using YemekhaneEntityLayer.Entities;
 namespace YemekhaneDataAccesLayer.Context
 {
     public class YemekhaneContext:DbContext
-    {          // Server=.\\SQLEXPRESS kişisel veritabanı bağlantısı 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;initial Catalog=yemekhanePProjesiDB;Integrated Security=true;Encrypt=True;TrustServerCertificate=True;");
+    {          // of vtys bağlnatı
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;initial Catalog=yemekhanePProjesiDB;Integrated Security=true;Encrypt=True;TrustServerCertificate=True;");
+        // yağmur vtys bağlantı 
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlServer("Server=LAPTOP-PV2VVQ07;initial Catalog=yemekhanePProjesiDB;Integrated Security=true;Encrypt=True;TrustServerCertificate=True;");
         public DbSet<Calisan> Calisanlar { get; set; }
         public DbSet<Okutmalar> Okutmalar { get; set; }
         public DbSet<Admin> Adminler { get; set; }
-        // public YemekhaneContext(DbContextOptions<YemekhaneContext> options)
-        //: base(options)
-        // {
-        // }
-
-        // public DbSet<Calisan> Calisanlar { get; set; }
-        // public DbSet<Okutmalar> Okutmalar { get; set; }
-        // public DbSet<Admin> Adminler { get; set; }
+    
     }
 }
