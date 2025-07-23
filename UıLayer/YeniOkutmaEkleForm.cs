d283ca2972dbfe3f179b2c97ba27c4a24444b010
Yamokay <yagmurkaya1212@gmail.com>
2025-07-23 20:28:06 +0300

    okuma ekleme yapılacak falan

---
+++ UıLayer/YeniOkutmaEkleForm.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UıLayer
+{
+    public partial class YeniOkutmaEkleForm : Form
+    {
+        private int calisanID;
+        public YeniOkutmaEkleForm()
+        {
+            InitializeComponent();
+            this.calisanID = calisanID;
+        }
+
+        private void YeniOkutmaEkleForm_Load(object sender, EventArgs e)
+        {
+        
+        }
+    }
+}
