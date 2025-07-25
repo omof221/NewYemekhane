24fd6f6342d82fd6a47c6cf1115c45c9b9a32a8f
ömer yaşar <omeryasar745@gmail.com>
2025-07-25 18:54:03 +0300

    sın inş

--- UıLayer/kartKullanıcıEkleme.Designer.cs
+++ UıLayer/kartKullanıcıEkleme.Designer.cs
@@ -30,7 +30,6 @@ namespace UıLayer
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
             dataGridView1 = new DataGridView();
             btnKaydet = new Button();
             txtIsim = new TextBox();
@@ -39,10 +38,13 @@ namespace UıLayer
             label2 = new Label();
             label3 = new Label();
             label4 = new Label();
-            timer1 = new System.Windows.Forms.Timer(components);
-            timer2 = new System.Windows.Forms.Timer(components);
             maskedTextBoxKartID = new MaskedTextBox();
             label1 = new Label();
+            comboBoxAktiflik = new ComboBox();
+            label5 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
@@ -52,12 +54,14 @@ namespace UıLayer
             dataGridView1.Location = new Point(12, 12);
             dataGridView1.Name = "dataGridView1";
             dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(591, 295);
+            dataGridView1.Size = new Size(840, 299);
             dataGridView1.TabIndex = 0;
+            dataGridView1.CellClick += dataGridView1_CellClick;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
             // btnKaydet
             // 
-            btnKaydet.Location = new Point(666, 256);
+            btnKaydet.Location = new Point(105, 566);
             btnKaydet.Name = "btnKaydet";
             btnKaydet.Size = new Size(255, 29);
             btnKaydet.TabIndex = 1;
@@ -67,21 +71,21 @@ namespace UıLayer
             // 
             // txtIsim
             // 
-            txtIsim.Location = new Point(745, 104);
+            txtIsim.Location = new Point(183, 373);
             txtIsim.Name = "txtIsim";
             txtIsim.Size = new Size(176, 27);
             txtIsim.TabIndex = 3;
             // 
             // txtGorevv
             // 
-            txtGorevv.Location = new Point(745, 201);
+            txtGorevv.Location = new Point(183, 470);
             txtGorevv.Name = "txtGorevv";
             txtGorevv.Size = new Size(176, 27);
             txtGorevv.TabIndex = 5;
             // 
             // txtSoyad
             // 
-            txtSoyad.Location = new Point(745, 157);
+            txtSoyad.Location = new Point(183, 426);
             txtSoyad.Name = "txtSoyad";
             txtSoyad.Size = new Size(176, 27);
             txtSoyad.TabIndex = 4;
@@ -89,7 +93,7 @@ namespace UıLayer
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(666, 107);
+            label2.Location = new Point(104, 376);
             label2.Name = "label2";
             label2.Size = new Size(36, 20);
             label2.TabIndex = 7;
@@ -98,7 +102,7 @@ namespace UıLayer
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(666, 164);
+            label3.Location = new Point(104, 433);
             label3.Name = "label3";
             label3.Size = new Size(48, 20);
             label3.TabIndex = 8;
@@ -108,7 +112,7 @@ namespace UıLayer
             // label4
             // 
             label4.AutoSize = true;
-            label4.Location = new Point(667, 208);
+            label4.Location = new Point(105, 477);
             label4.Name = "label4";
             label4.Size = new Size(47, 20);
             label4.TabIndex = 9;
@@ -116,7 +120,7 @@ namespace UıLayer
             // 
             // maskedTextBoxKartID
             // 
-            maskedTextBoxKartID.Location = new Point(745, 10);
+            maskedTextBoxKartID.Location = new Point(184, 335);
             maskedTextBoxKartID.Mask = "0000000000";
             maskedTextBoxKartID.Name = "maskedTextBoxKartID";
             maskedTextBoxKartID.Size = new Size(125, 27);
@@ -125,17 +129,69 @@ namespace UıLayer
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(633, 12);
+            label1.Location = new Point(72, 337);
             label1.Name = "label1";
             label1.Size = new Size(106, 20);
             label1.TabIndex = 13;
             label1.Text = "Çalışan Kart ID";
             // 
+            // comboBoxAktiflik
+            // 
+            comboBoxAktiflik.FormattingEnabled = true;
+            comboBoxAktiflik.Location = new Point(183, 520);
+            comboBoxAktiflik.Name = "comboBoxAktiflik";
+            comboBoxAktiflik.Size = new Size(148, 28);
+            comboBoxAktiflik.TabIndex = 14;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(105, 528);
+            label5.Name = "label5";
+            label5.Size = new Size(54, 20);
+            label5.TabIndex = 15;
+            label5.Text = "Durum";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(382, 566);
+            button1.Name = "button1";
+            button1.Size = new Size(255, 29);
+            button1.TabIndex = 16;
+            button1.Text = "Güncelle";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(104, 601);
+            button2.Name = "button2";
+            button2.Size = new Size(255, 29);
+            button2.TabIndex = 17;
+            button2.Text = "Satırı Temizle";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(382, 601);
+            button3.Name = "button3";
+            button3.Size = new Size(255, 29);
+            button3.TabIndex = 18;
+            button3.Text = "Sil";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
             // kartKullanıcıEkleme
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(956, 569);
+            ClientSize = new Size(1270, 684);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(comboBoxAktiflik);
             Controls.Add(label1);
             Controls.Add(maskedTextBoxKartID);
             Controls.Add(label4);
@@ -148,9 +204,7 @@ namespace UıLayer
             Controls.Add(dataGridView1);
             Name = "kartKullanıcıEkleme";
             Text = "kartKullanıcıEkleme";
-            //yoruma aldım
-            /*Load += kartKullanıcıEkleme_Load*/
-            ;
+            Load += kartKullanıcıEkleme_Load;
             Shown += kartKullanıcıEkleme_Shown;
             ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
@@ -167,9 +221,12 @@ namespace UıLayer
         private Label label2;
         private Label label3;
         private Label label4;
-        private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Timer timer2;
         private MaskedTextBox maskedTextBoxKartID;
         private Label label1;
+        private ComboBox comboBoxAktiflik;
+        private Label label5;
+        private Button button1;
+        private Button button2;
+        private Button button3;
     }
 }
